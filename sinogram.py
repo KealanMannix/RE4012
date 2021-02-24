8bb31d81baf36a9ba8c6ce3be0ed1d50f1bcb1c1
KealanMannix <kealanmannix13@gmail.com>
2021-02-24 14:38:49 +0000

    sinogram code updated

--- sinogram.py
+++ sinogram.py
@@ -1,55 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import PIL
 import imageio
 import scipy
 from scipy import fftpack
 from skimage.transform import rotate
-import radontea
 from skimage.transform import iradon
 
 sinogram = np.array(Image.open("sinogram.png"))
+
+print(sinogram.shape)
 
 red = sinogram[...,0]
 green = sinogram[...,1]
 blue = sinogram[...,2]
 
-recon = np.dstack((red,green,blue))
+#recon = np.dstack((red,green,blue))
 
 red_fft = fftpack.rfft(red,axis=1)
 green_fft = fftpack.rfft(green,axis=1)
 blue_fft = fftpack.rfft(blue,axis=1)
+
+ramp = np.floor(np.arange(0.5, red_fft.shape[1]//2 + 0.1,0.5))
+window = np.hamming(red_fft.shape[1])
+red_fft = red_fft*ramp*window
+green_fft = green_fft*ramp*window
+blue_fft = blue_fft*ramp*window
 
 recon_red = fftpack.irfft(red_fft,axis=1)
 recon_green = fftpack.irfft(green_fft,axis=1)
 recon_blue = fftpack.irfft(blue_fft,axis=1)
 
 
-recon_image = np.zeros((recon_red.shape[0],recon_red.shape[1]))
-theta = 180.0/recon_red.shape[0]
-for i in range(recon_red.shape[0]):
-    var = np.tile(recon_red[i],(recon_red.shape[0],1))
-    var = rotate(var,theta*i)
-    recon_image += var
-    
-recon_image2 = np.zeros((recon_red.shape[0],recon_red.shape[1]))
-theta2 = 180.0/recon_red.shape[0]
-for i2 in range(recon_red.shape[0]):
-    var2 = np.tile(recon_red[i],(recon_red.shape[0],1))
-    var2 = rotate(var2,theta2*i2)
-    recon_image2 += var2
-    
-recon_image3 = np.zeros((recon_red.shape[0],recon_red.shape[1]))
-theta3 = 180.0/recon_red.shape[0]
-for i3 in range(recon_red.shape[0]):
-    var3 = np.tile(recon_red[i],(recon_red.shape[0],1))
-    var3 = rotate(var3,theta3*i3)
-    recon_image3 += var3
 
-    
-econ = np.dstack((recon_image,recon_image2,recon_image3))
 
-#final_im = iradon(recon)
+def reconstruct(image):
+    recon_image = np.zeros((image.shape[1],image.shape[1]))
+    steps = 360
+    dtheta = -180/steps
+    
+    for i in range(steps):
+        temp = np.tile(image[i,:],(image.shape[1],1))
+        temp = rotate(temp, dtheta*i)
+        #temp = np.pad(temp,((0,0),(149,149)),'constant')
+        recon_image += temp
+
+    return recon_image
+
+zero_image = np.zeros((red.shape[1],red.shape[1]))
+
+r = reconstruct(recon_red)
+g = reconstruct(recon_green)
+b = reconstruct(recon_blue)
+
+def convert_to_8bit(ch):
+    chi, clo = ch.max(),ch.min()
+    chnorm = 255*(ch-clo)/(chi-clo)
+    ch8bit = np.floor(chnorm).astype('uint8')
+    return ch8bit
+
+
+r = convert_to_8bit(r)
+g = convert_to_8bit(g)
+b = convert_to_8bit(b)
+
+final_image = np.dstack((r,g,b))
+
+
 plt.figure()
-plt.imshow(recon_image)
+#plt.imshow(b,cmap=plt.cm.gray)
+plt.imshow(final_image)
+plt.axis("off")
 
