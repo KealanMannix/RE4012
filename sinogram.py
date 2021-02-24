65ed1b53d31682c37c083beab7e1a12e97be8f6d
KealanMannix <kealanmannix13@gmail.com>
2021-02-24 19:46:59 +0000

    sinogram code updated

--- sinogram.py
+++ sinogram.py
@@ -1,58 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import PIL
-import imageio
-import scipy
 from scipy import fftpack
 from skimage.transform import rotate
-from skimage.transform import iradon
+
 
 sinogram = np.array(Image.open("sinogram.png"))
-
-print(sinogram.shape)
 
 red = sinogram[...,0]
 green = sinogram[...,1]
 blue = sinogram[...,2]
 
-#recon = np.dstack((red,green,blue))
 
-red_fft = fftpack.rfft(red,axis=1)
-green_fft = fftpack.rfft(green,axis=1)
-blue_fft = fftpack.rfft(blue,axis=1)
+def get_im_size(sinogram):
+    length = sinogram.shape[1]
+    zeros = True
+    i=0
+    num_zeros = 0
+    while (zeros == True):
+        if (sinogram[1,i] == 0):
+            num_zeros += 1
+        else:
+            zeros = False
+        i+=1
+    length = length - (num_zeros*2)
+    return length
 
-ramp = np.floor(np.arange(0.5, red_fft.shape[1]//2 + 0.1,0.5))
-window = np.hamming(red_fft.shape[1])
-red_fft = red_fft*ramp*window
-green_fft = green_fft*ramp*window
-blue_fft = blue_fft*ramp*window
+def get_fft(image):
+    image = fftpack.rfft(image,axis=1)
+    return image
 
-recon_red = fftpack.irfft(red_fft,axis=1)
-recon_green = fftpack.irfft(green_fft,axis=1)
-recon_blue = fftpack.irfft(blue_fft,axis=1)
+def ramp_filter(fft):
+    ramp = np.floor(np.arange(0.5, fft.shape[1]//2 + 0.1,0.5))
+    return ramp*fft
 
+def window(fft):
+    window = np.hamming(fft.shape[1])
+    return window*fft
 
-
-
-def reconstruct(image):
-    recon_image = np.zeros((image.shape[1],image.shape[1]))
-    steps = 360
-    dtheta = -180/steps
-    
-    for i in range(steps):
-        temp = np.tile(image[i,:],(image.shape[1],1))
-        temp = rotate(temp, dtheta*i)
-        #temp = np.pad(temp,((0,0),(149,149)),'constant')
-        recon_image += temp
-
-    return recon_image
-
-zero_image = np.zeros((red.shape[1],red.shape[1]))
-
-r = reconstruct(recon_red)
-g = reconstruct(recon_green)
-b = reconstruct(recon_blue)
+def get_ifft(fft):
+    ifft = fftpack.irfft(fft,axis=1)
+    return ifft
 
 def convert_to_8bit(ch):
     chi, clo = ch.max(),ch.min()
@@ -60,16 +48,60 @@ def convert_to_8bit(ch):
     ch8bit = np.floor(chnorm).astype('uint8')
     return ch8bit
 
+def reconstruct(fft,length):
+    image = fftpack.irfft(fft,axis=1)
+    
+    recon_image = np.zeros((image.shape[1],image.shape[1]))
+    steps = 360
+    dtheta = 180/steps
+    
+    for i in range(steps):
+        temp = np.tile(image[i,:],(image.shape[1],1))
+        temp = rotate(temp, dtheta*i)
+        recon_image += temp
+        
+    padding_width = (image.shape[1]-length)/2
+    left = int(padding_width)
+    right = int(padding_width+length)
+    
+    crop_image = recon_image[left:right, left:right]
+    
+    crop_image = convert_to_8bit(crop_image)
 
-r = convert_to_8bit(r)
-g = convert_to_8bit(g)
-b = convert_to_8bit(b)
-
-final_image = np.dstack((r,g,b))
+    return crop_image
 
 
+def build_image(r,g,b,length):
+    red_ch = reconstruct(r, length)
+    green_ch = reconstruct(g, length)
+    blue_ch = reconstruct(b, length)
+    
+    image = np.dstack((red_ch,green_ch,blue_ch))
+    return image
+
+length = get_im_size(red)
+
+##Reconstruct with no filtering
+#no_filter = build_image(red,green,blue,length)
+
+##Reconstruct with Ramp filter
+red_ramp = ramp_filter(get_fft(red))
+green_ramp = ramp_filter(get_fft(green))
+blue_ramp = ramp_filter(get_fft(blue))
+
+#ramp_filter = build_image(red_ramp,green_ramp,blue_ramp,length)
+
+##Reconstruct with ramp filter and window
+red_win = window(red_ramp)
+green_win = window(green_ramp)
+blue_win = window(blue_ramp)
+
+window_ramp = build_image(red_win,green_win,blue_win,length)
+
+
+#img = Image.fromarray(window_ramp, 'RGB')
+#img.show(vmin=0,vmax=255)
 plt.figure()
-#plt.imshow(b,cmap=plt.cm.gray)
-plt.imshow(final_image)
+plt.imshow(window_ramp,vmin=0,vmax=255)
 plt.axis("off")
 
