ab30d0af09d442184220842674f5d480ae32d74f
KealanMannix <kealanmannix13@gmail.com>
2021-02-25 12:36:52 +0000

    sinogram code updated

--- sinogram.py
+++ sinogram.py
@@ -4,14 +4,18 @@ from PIL import Image
 from scipy import fftpack
 from skimage.transform import rotate
 
-
+#Load in sinogram
 sinogram = np.array(Image.open("sinogram.png"))
 
+#split sinogram into 3 color channel
 red = sinogram[...,0]
 green = sinogram[...,1]
 blue = sinogram[...,2]
 
 
+#Find the width of the image to be reconstructed by counting leading zeros in 
+#sinogram image 
+#assumes aspect ratio is 1:1
 def get_im_size(sinogram):
     length = sinogram.shape[1]
     zeros = True
@@ -26,28 +30,29 @@ def get_im_size(sinogram):
     length = length - (num_zeros*2)
     return length
 
+#Get fft of an image
 def get_fft(image):
     image = fftpack.rfft(image,axis=1)
     return image
 
+#Mulyiply a frequency domain image by ramp filter
 def ramp_filter(fft):
     ramp = np.floor(np.arange(0.5, fft.shape[1]//2 + 0.1,0.5))
     return ramp*fft
 
+#Mulyiply a frequency domain image by hamming window
 def window(fft):
     window = np.hamming(fft.shape[1])
     return window*fft
 
-def get_ifft(fft):
-    ifft = fftpack.irfft(fft,axis=1)
-    return ifft
-
+#Convert images to 8 bit values (between 0 and 255)
 def convert_to_8bit(ch):
     chi, clo = ch.max(),ch.min()
     chnorm = 255*(ch-clo)/(chi-clo)
     ch8bit = np.floor(chnorm).astype('uint8')
     return ch8bit
 
+#Reconstruct the image by backprojects (length is size of image to be reconstructed)
 def reconstruct(fft,length):
     image = fftpack.irfft(fft,axis=1)
     
@@ -71,6 +76,7 @@ def reconstruct(fft,length):
     return crop_image
 
 
+#Build RGB image from 3 frequency domain channels
 def build_image(r,g,b,length):
     red_ch = reconstruct(r, length)
     green_ch = reconstruct(g, length)
@@ -82,14 +88,14 @@ def build_image(r,g,b,length):
 length = get_im_size(red)
 
 ##Reconstruct with no filtering
-#no_filter = build_image(red,green,blue,length)
+no_filter = build_image(red,green,blue,length)
 
 ##Reconstruct with Ramp filter
 red_ramp = ramp_filter(get_fft(red))
 green_ramp = ramp_filter(get_fft(green))
 blue_ramp = ramp_filter(get_fft(blue))
 
-#ramp_filter = build_image(red_ramp,green_ramp,blue_ramp,length)
+ramp_filter = build_image(red_ramp,green_ramp,blue_ramp,length)
 
 ##Reconstruct with ramp filter and window
 red_win = window(red_ramp)
@@ -99,9 +105,21 @@ blue_win = window(blue_ramp)
 window_ramp = build_image(red_win,green_win,blue_win,length)
 
 
-#img = Image.fromarray(window_ramp, 'RGB')
-#img.show(vmin=0,vmax=255)
-plt.figure()
+
+plt.figure(1)
+plt.imshow(no_filter,vmin=0,vmax=255)
+plt.axis("off")
+plt.title('Image Reconstructed Without Ramp Filtering or Windowing')
+plt.show()
+
+plt.figure(2)
+plt.imshow(ramp_filter,vmin=0,vmax=255)
+plt.axis("off")
+plt.title('Image Reconstructed With Ramp Filtering and No Windowing')
+plt.show()
+
+plt.figure(3)
 plt.imshow(window_ramp,vmin=0,vmax=255)
 plt.axis("off")
-
+plt.title('Image Reconstructed With Ramp Filtering and Hamming Windowing')
+plt.show()
